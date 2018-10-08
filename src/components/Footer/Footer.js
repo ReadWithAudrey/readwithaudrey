72cca1b60242410837312d4700d05ca39f62868d
Matt Hall <matthall8@gmail.com>
2018-10-08 14:54:40 +0100

    Created footer component #54

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledFooter, FooterNav, Copyright } from './Footer.style'
+
+const Footer = () => (
+  <StyledFooter>
+    <FooterNav>Home | Our Story | FAQs | Contact Us</FooterNav>
+    <Copyright>&copy; Audrey | Terms | Privacy</Copyright>
+  </StyledFooter>
+)
+
+export default Footer
---
+++ src/components/Footer/Footer.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.footer`
+  background: #fff;
+`
+
+export const FooterNav = styled.span`
+  background: #fff;
+`
+
+export const Copyright = styled.span`
+  background: #fff;
+`
