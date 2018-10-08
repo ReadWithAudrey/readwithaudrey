bacfbcf50f4b98633309738ec881f3dc433dbc25
Ben Newman <benjamin-newman@live.co.uk>
2018-10-08 16:48:13 +0100

    Mailgun now sending a pdf > #41

--- server/emails/mailgun/pairingEmail.js
+++ server/emails/mailgun/pairingEmail.js
@@ -3,12 +3,18 @@ require('dotenv').config()
 var api_key = process.env.mailgunApiKey
 var domain = process.env.domain
 var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain })
+var path = require('path')
+var user1 = {}
 
+var user2 = {}
+
+var filepath = path.join(__dirname, 'cv.pdf')
 var data = {
   from: 'Rob <readwithaudrey.data@gmail.com>',
   to: '	benjamin-newman@live.co.uk',
   subject: 'Hello Ben! Welcome to readwithaudrey',
   text: 'Testing some Mailgun awesomeness!',
+  attachment: filepath,
 }
 
 mailgun.messages().send(data, function(error, body) {
