a1759651cf18cd1c31e59c69fd7c6518d6610a16
Kate Beard <asouthernbellein@gmail.com>
2018-10-09 15:58:45 +0100

    Finish first draft about page

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -8,12 +8,13 @@ const Aboutpage = () => (
       <h2 className="f2 pink tc">What is Audrey?</h2>
       <p className="tc lh-copy">Audrey is a platform which matches readers with listeners to read thought-provoking fiction and non-fiction aloud together.</p>
     </div>
+    <div id="who-for">
+      <h2 className="f2 pink tc">Who is it for?</h2>
+      <p className="tc lh-copy">People who enjoy reading and social connection, and like to hear diverse viewpoints.</p>
+    </div>
     <div id="who-with">
       <h2 className="f2 pink tc">Who do I read with?</h2>
       <p className="tc lh-copy">We match people with shared interests, but diverse life journeys. You can choose to be either the reader or listener.</p>
-    </div>
-    <div id="as-seen-on" className="ba bw2 br2 b--pink w-two-thirds h-25">
-
     </div>
   </AboutpageStyled>
 )
