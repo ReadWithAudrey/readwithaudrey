5bc743c2074d28e5d29b465aa2138a93872e7e91
Kate Beard <asouthernbellein@gmail.com>
2018-10-09 16:44:58 +0100

    Add logos to ASO page

--- src/components/ASOpage/ASOpage.style.js
+++ src/components/ASOpage/ASOpage.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const ASOpageStyled = styled.div.attrs({
-  className: "bg-white flex flex-column items-center justify-around ph4 pv5 montserrat w-100",
+  className: "bg-white flex flex-column items-center justify-between ph4 pt5 pb6 montserrat w-100",
 })`
   height: 100vh;
 `
