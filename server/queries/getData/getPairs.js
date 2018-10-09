2a6eb86cf2bcf0a5f3115df82b62ef702d474c05
Ben Newman <benjamin-newman@live.co.uk>
2018-10-09 23:23:37 +0100

    airtable linked to email > #74

--- server/queries/getData/getPairs.js
+++ server/queries/getData/getPairs.js
@@ -1,30 +1,42 @@
 require('dotenv').config();
-
 const base = require('../../dbConnection');
 
-base('pairings')
-  .select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    view: 'Grid view',
-  })
-  .eachPage(
-    (records, fetchNextPage) => {
-      // This function (`page`) will get called for each page of records.
+const getNewPairs = new Promise((resolve) => {
+  base('pairings')
+    .select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      filterByFormula: 'AND({confirm_pairing}, NOT({pairing_email_sent}))',
+      fields: [
+        'user1_name',
+        'user1_email',
+        'user1_bio',
+        'user2_name',
+        'user2_email',
+        'user2_bio',
+        'book_name',
+        'book_author',
+        'book_attachments',
+        'book_bio',
+        'mini_book_bio',
+      ],
+    })
+    .eachPage(
+      (records, fetchNextPage) => {
+        // This function (`page`) will get called for each page of records.
+        resolve(records);
 
-      records.forEach((record) => {
-        console.log(record);
-        console.log('Retrieved', record.get('id'));
-      });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+      },
+    );
+});
 
-      // To fetch the next page of records, call `fetchNextPage`.
-      // If there are more records, `page` will get called again.
-      // If there are no more records, `done` will get called.
-      fetchNextPage();
-    },
-    (err) => {
-      if (err) {
-        console.error(err);
-      }
-    },
-  );
+module.exports = getNewPairs;
