9dfd68e86da99c91c089c5e4b0c812918a474a5c
Ben Newman <benjamin-newman@live.co.uk>
2018-10-09 12:41:48 +0100

    reorganised files > #62

--- server/queries/getData/getPairs.js
+++ server/queries/getData/getPairs.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
-const Airtable = require('airtable');
-
-Airtable.configure({
-  endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
-
-const base = Airtable.base(process.env.AIRTABLE_BASE);
+const base = require('../../dbConnection');
 
 base('pairings')
   .select({
