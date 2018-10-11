473580b113ff00183643283a6cb41060d7f096bb
Matt Hall <matthall8@gmail.com>
2018-10-11 14:15:08 +0100

    Updated naming conventions as per other sections

--- src/pageSections/Examples/Examples.js
+++ src/pageSections/Examples/Examples.js
@@ -1,22 +1,40 @@
 import React from 'react'
-import ExamplespageStyled from './Examples.style'
+import StyledExamples from './Examples.style'
 import { Button } from '../../components/index'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
 const Examples = () => (
-  <ExamplespageStyled>
+  <StyledExamples>
     <h1 className="f2 pink tc">Our latest readers</h1>
-    <h2 className="f4 tc">We take great pride in our reading pairings. Here are some of our most recent:</h2>
+    <h2 className="f4 tc">
+      We take great pride in our reading pairings. Here are some of our most
+      recent:
+    </h2>
     <ul className="tl">
-      <li className="pv2">A relationship therapist and medical doctor read about attachment theory</li>
-      <li className="pv2">A retired judge and a history student read Darkest Hour, by Anthony McCarten</li>
-      <li className="pv2">A professional musician and a teaching assistant read Musicophilia, by Oliver Sacks</li>
-      <li className="pv2">A young playwright and an experienced radio presenter read Wonder, by R J Palacio</li>
+      <li className="pv2">
+        A relationship therapist and medical doctor read about attachment theory
+      </li>
+      <li className="pv2">
+        A retired judge and a history student read Darkest Hour, by Anthony
+        McCarten
+      </li>
+      <li className="pv2">
+        A professional musician and a teaching assistant read Musicophilia, by
+        Oliver Sacks
+      </li>
+      <li className="pv2">
+        A young playwright and an experienced radio presenter read Wonder, by R
+        J Palacio
+      </li>
     </ul>
     <p className="f4 tc">Are you ready to join the Audrey community?</p>
-    <Button><Link to="/Form1/" className="no-underline white">Get started</Link></Button>
-  </ExamplespageStyled>
+    <Button>
+      <Link to="/Form1/" className="no-underline white">
+        Get started
+      </Link>
+    </Button>
+  </StyledExamples>
 )
 
 Examples.propTypes = {
--- src/pageSections/Examples/Examples.style.js
+++ src/pageSections/Examples/Examples.style.js
@@ -1,8 +1,9 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
-const ExamplespageStyled = styled.div.attrs({
-  className: "bg-white flex flex-column items-center justify-between ph4 c-padding montserrat w-100",
+const StyledExamples = styled.div.attrs({
+  className:
+    'bg-white flex flex-column items-center justify-between ph4 c-padding montserrat w-100',
 })`
   height: 100vh;
 `
-export default ExamplespageStyled;
+export default StyledExamples
