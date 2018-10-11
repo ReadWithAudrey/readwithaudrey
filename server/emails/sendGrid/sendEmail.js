0b727ba255a1e7eed98355c97a0b336895e87bb5
DenisKent <kentdenis6@hotmail.co.uk>
2018-10-11 17:39:32 +0100

    customised emails sending > #98

--- server/emails/sendGrid/sendEmail.js
+++ server/emails/sendGrid/sendEmail.js
@@ -4,8 +4,12 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
-  to: 'readwithaudrey.data@gmail.com',
-  from: 'test@example.com',
-  template_id: 'd-74d18868e6d84290860df912d769464d',
+  to: ['readwithaudrey.data@gmail.com'],
+  from: 'readwithaudrey.data@gmail.com',
+  templateId: 'd-74d18868e6d84290860df912d769464d',
+  dynamic_template_data: {
+    user1: 'Denis',
+    user2: 'Ben',
+  },
 };
 sgMail.send(msg);
