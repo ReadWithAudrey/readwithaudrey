b799fbf3d62a8dc7d24f814096ffe78122d9e32a
Matt Hall <matthall8@gmail.com>
2018-10-11 16:55:18 +0100

    Added contact page to footer #49

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import { TextBox, Layout, Label, TextArea, Button, Input } from '../components/'
+import {
+  TextBox,
+  Layout,
+  Label,
+  TextArea,
+  Button,
+  InputBox,
+} from '../../components/'
 
 const ContactUs = () => {
   return (
     <Layout>
       <h1 className="f3 pink tc montserrat mb3">Contact Us</h1>
       <TextBox>
-        We would love to hear from you. Get in touch either by filling out the
-        contact form below or contacting us at{' '}
-        <a href="mailto:readwithaudrey@gmail.com">readwithaudrey@gmail.com</a>
+        We would love to hear from you. Get in touch by filling out the form
+        below:
       </TextBox>
       <form method="POST" action="http://localhost:5000/formPart1">
         <Label>Your Name</Label>
-        <Input placeholder="Your Name" />
+        <InputBox placeholder="Your Name" />
         <Label>Your Message</Label>
         <TextArea placeholder="Your Message" />
         <Link to="/thankyou">
