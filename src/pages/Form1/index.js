9fd72ee71fa27db998a7c959c4d1f2436d7b9e4f
Ben Newman <benjamin-newman@live.co.uk>
2018-10-11 22:04:29 +0100

    Front end component testing complete. Links and data posting test still required > #106

--- src/pages/Form1/index.js
+++ src/pages/Form1/index.js
@@ -18,12 +18,12 @@ const Form1 = ({ data }) => {
       <form method="POST" action="http://localhost:5000/formPart1">
         <h1 className="f3 pink tc montserrat mb3">Your Details</h1>
         <StatusBar type="active">1. Basic Details</StatusBar>
-        <StatusBar>
-          <Link to="/Form2">2. Further Details</Link>
-        </StatusBar>
-        <StatusBar>
-          <Link to="/Form3">3. Your Bio</Link>
-        </StatusBar>
+        <Link to="/Form2">
+          <StatusBar>2. Further Details</StatusBar>
+        </Link>
+        <Link to="/Form3">
+          <StatusBar>3. Your Bio</StatusBar>
+        </Link>
         <TextBox>
           Welcome to Audrey, thank you for taking the steps to join our
           community.
@@ -38,9 +38,9 @@ const Form1 = ({ data }) => {
           <Button type="register">Continue</Button>
         </Link>
       </form>
-      <TextBox>
-        <Link to="/">Go back to the homepage</Link>
-      </TextBox>
+      <Link to="/">
+        <TextBox>Go back to the homepage</TextBox>
+      </Link>
     </Layout>
   )
 }
