8d8f5dee8a556cf91e7b21b603362a3302f5c99f
Kate Beard <asouthernbellein@gmail.com>
2018-10-11 12:39:54 +0100

    Change button highlight color

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 
 export const StyledButton = styled.button.attrs({
-  className: 'bg-pink hover-bg-yellow white pa2 br2 w-50 h3 f3 montserrat',
+  className: 'bg-pink hover-bg-orange white pa2 br2 w-50 h3 f3 montserrat',
 })`
   border: none;
 `
 
 export const HeaderButton = styled.button.attrs({
-  className: "bg-green hover-bg-yellow pa2 white br2 w-third f5 montserrat"})`
+  className: "bg-green hover-bg-pink pa2 white br2 w-third f5 montserrat"})`
   border: 2px solid white;
 `
 
 export const RegisterButton = styled.button.attrs({
-  className: 'bg-pink hover-bg-yellow white pa2 br2 w-50 h3 f3 center',
+  className: 'bg-pink hover-bg-orange white pa2 br2 w-50 h3 f3 center',
 })`
   border: 2px solid white;
   display: block;
