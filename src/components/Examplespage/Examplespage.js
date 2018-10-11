4e6f5d44a4c165c30b716a0c3d601dbbb7dc3836
Kate Beard <asouthernbellein@gmail.com>
2018-10-11 12:05:47 +0100

    Add examples page layout, unfix footer

---
+++ src/components/Examplespage/Examplespage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ExamplespageStyled from './Examplespage.style'
+import PropTypes from 'prop-types'
+
+const Examplespage = () => (
+  <ExamplespageStyled>
+
+  </ExamplespageStyled>
+)
+
+Examplespage.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+export default Examplespage
---
+++ src/components/Examplespage/Examplespage.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+const ExamplespageStyled = styled.div.attrs({
+  className: "bg-light-green flex flex-column items-center justify-around ph4 c-padding montserrat w-100",
+})`
+  height: 100vh;
+`
+export default ExamplespageStyled;
