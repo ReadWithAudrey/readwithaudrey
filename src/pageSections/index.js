d2000ae04ca1dacb039444f43116e6679d4ebe59
Matt Hall <matthall8@gmail.com>
2018-10-11 12:26:06 +0100

    Removed page sections from components and moved to pageSections

---
+++ src/pageSections/index.js
@@ -0,0 +1,6 @@
+import WelcomeScreen from './Homepage/Homepage'
+import AboutAudrey from './Aboutpage/Aboutpage'
+import AsSeenOn from './ASOpage/ASOpage'
+import HowItWorks from './Howpage/Howpage'
+
+export { WelcomeScreen, AboutAudrey, AsSeenOn, HowItWorks }
