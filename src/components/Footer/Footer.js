298ec9d3c4b9a7275cd6d789f477e38fdebc5e15
Matt Hall <matthall8@gmail.com>
2018-10-12 17:37:20 +0100

    Removed footer navigation and moved contact us to footer submenu  #129

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { StyledFooter, FooterNav, Copyright } from './Footer.style'
+import { StyledFooter, Copyright } from './Footer.style'
 
 const Footer = () => (
   <StyledFooter>
-    <FooterNav>
-      <Link to="/" className="no-underline white ph1 ">
-        Home
-      </Link>
-      <Link to="/story/" className="no-underline white ph1">
-        Our Story
-      </Link>
-      <Link to="/faq/" className="no-underline white ph1">
-        FAQs
-      </Link>
-      <Link to="/contact/" className="no-underline white ph1">
-        Contact Us
-      </Link>
-    </FooterNav>
     <Copyright>
       &copy; Audrey
-      <Link to="/Terms/" className="no-underline black ph1">
+      <Link to="/terms/" className="no-underline black ph1">
         Terms
       </Link>
-      <Link to="/Privacy/" className="no-underline black ph1">
+      <Link to="/privacy/" className="no-underline black ph1">
         Privacy
+      </Link>
+      <Link to="/contact/" className="no-underline black ph1">
+        Contact
       </Link>
     </Copyright>
   </StyledFooter>
