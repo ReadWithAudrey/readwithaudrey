a7432692354af2ebab132b340db3d4f621c7b89b
Ben Newman <benjamin-newman@live.co.uk>
2018-10-15 12:52:23 +0100

    minor changes > #106

--- src/pages/Story/index.test.js
+++ src/pages/Story/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Story from './index'
+import Story from './index.js'
 
 describe('Story page snapshot test', () =>
   it('renders correctly', () => {
