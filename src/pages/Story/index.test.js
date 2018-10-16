5fa68fc4e3457625b7cb215c2c9541a48afc5ace
Ben Newman <benjamin-newman@live.co.uk>
2018-10-16 11:07:08 +0100

    removed story tests > #106

--- src/pages/Story/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Story from './index.js'
-
-describe('Story page snapshot test', () =>
-  it('renders correctly', () => {
-    const tree = renderer.create(<Story />).toJSON()
-    expect(tree).toMatchSnapshot()
-  }))
