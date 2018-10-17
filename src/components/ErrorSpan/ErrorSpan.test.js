862cb41ee0cd3244727e976ffedbbdf2da45a266
Kate Beard <asouthernbellein@gmail.com>
2018-10-17 16:58:41 +0100

    Add error span component test

--- src/components/ErrorSpan/ErrorSpan.test.js
+++ src/components/ErrorSpan/ErrorSpan.test.js
@@ -1,1 +1,9 @@
-// tests to go here
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ErrorSpan from './ErrorSpan'
+
+describe('Error span component snapshot test', () =>
+  it('renders correctly', () => {
+    const tree = renderer.create(<ErrorSpan />).toJSON()
+    expect(tree).toMatchSnapshot()
+  }))
