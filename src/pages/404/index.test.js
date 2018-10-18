f296beeee1f265353affa050a9faa067b386a69e
Matt Hall <matthall8@gmail.com>
2018-10-18 23:20:31 +0100

    Removed unnecessary tests

--- src/pages/404/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import NotFoundPage from './index.js'
-
-describe('NotFoundPage page snapshot test', () =>
-  it('renders correctly', () => {
-    const tree = renderer.create(<NotFoundPage />).toJSON()
-    expect(tree).toMatchSnapshot()
-  }))
