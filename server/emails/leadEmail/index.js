c10b26be5dda849f68b1ef16d96a1f0280e81773
DenisKent <kentdenis6@hotmail.co.uk>
2018-10-18 19:14:52 +0100

    lead emails sending > #91

---
+++ server/emails/leadEmail/index.js
@@ -0,0 +1,22 @@
+const { getLeads } = require('../../queries/getData');
+const { updateLeadEmailSent } = require('../../queries/postData');
+const { sendLeadEmail } = require('./sendLeadEmail');
+
+const sendLeadEmails = () => {
+  getLeads()
+    .then((leads) => {
+      if (leads.length > 0) {
+      const uniqueLeads = leads.filter(() => {
+        leads.forEach((lead) => {
+          sendLeadEmail(lead)
+            .then(() => updateLeadEmailSent(lead))
+            .then(res => console.log('success', res))
+            .catch(e => console.log('error for an individual lead', e.message));
+        });
+      }
+    }).catch(console.log);
+};
+
+sendLeadEmails();
+
+module.exports = sendLeadEmails;
