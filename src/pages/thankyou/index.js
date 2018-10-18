8d5052183b7a01a16debb337f4598a17a98414bc
Kate Beard <asouthernbellein@gmail.com>
2018-10-18 15:04:24 +0100

    New social icons, fix thank you page spacing

--- src/pages/thankyou/index.js
+++ src/pages/thankyou/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import facebook from '../../images/facebook.png'
-import email from '../../images/email.png'
-import twitter from '../../images/twitter.png'
+import facebook from '../../images/facebook.svg'
+import email from '../../images/gmail.svg'
+import twitter from '../../images/twitter.svg'
 
 import { TextBox, Layout } from '../../components/'
 
 const ThankYou = () => {
   return (
     <Layout>
-      <h1 className="f3 pink tc montserrat mb3">Great… you’re in… Welcome!</h1>
+      <h1 className="f3 pink tc montserrat mt5 mb4">Great… you’re in… Welcome!</h1>
       <TextBox>
         Thank you for completing the sign up. That’s the hard part done. Welcome
         to our community. We’re delighted to have you on board!
@@ -23,16 +23,17 @@ const ThankYou = () => {
         you to your reading partner
       </TextBox>
       <TextBox>
-        If you have any questions in the meantime, please
-        <Link to="/contact">contact us</Link>
+        If you have any questions in the meantime, please <Link to="/contact">contact us</Link>
       </TextBox>
-      <h2 className="f3 pink tc montserrat mb3">Help Spread The Word</h2>
+      <h2 className="f3 pink tc montserrat mt5 mb4">Help Spread The Word</h2>
       <TextBox>
         Why not share the story of Audrey and help us spread the word?
       </TextBox>
-      <img src={facebook} alt="Facebook" className="fl" />
-      <img src={twitter} alt="Twitter" className="fl" />
-      <img src={email} alt="Email" className="fl" />
+      <div id="social-logos" className="flex justify-around w-100 flex-ns ph6-ns pv4">
+        <img src={facebook} alt="Facebook" className="small-icon mw-25 h3-ns" />
+        <img src={twitter} alt="Twitter" className="small-icon mw-25 h3-ns" />
+        <img src={email} alt="Email" className="small-icon mw-25 h3-ns" />
+      </div>
       <TextBox>Have a great day!</TextBox>
       <TextBox>Rob and the Audrey team</TextBox>
       <Link to="/">
