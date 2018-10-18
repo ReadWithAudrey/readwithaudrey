f672ed22e877ab610f1db06823407e4b8e0dce15
Ben Newman <benjamin-newman@live.co.uk>
2018-10-18 23:08:05 +0100

    minor changes > #177

--- server/queries/postData/updatePairingEmailStatus.js
+++ server/queries/postData/updatePairingEmailStatus.js
@@ -1,22 +1,22 @@
-// require('dotenv').config();
-// const base = require('../../dbConnection');
-//
-// const updateSentStatus = ({ id }) => {
-//   console.log('Updating sent status');
-//   return new Promise((resolve, reject) => {
-//     base('pairings').update(
-//       id,
-//       {
-//         pairing_email_sent: true,
-//       },
-//       (err) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve();
-//         }
-//       },
-//     );
-//   });
-// };
-// module.exports = updateSentStatus;
+require('dotenv').config();
+const base = require('../../dbConnection');
+
+const updateSentStatus = ({ id }) => {
+  console.log('Updating sent status');
+  return new Promise((resolve, reject) => {
+    base('pairings').update(
+      id,
+      {
+        pairing_email_sent: true,
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+};
+module.exports = updateSentStatus;
