be4bdc947a060666611783a3b38a611a4ffc7359
Kate Beard <asouthernbellein@gmail.com>
2018-10-18 15:45:48 +0100

    Update background colors of Testimonials and Examples

--- src/pageSections/Examples/Examples.style.js
+++ src/pageSections/Examples/Examples.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const StyledExamples = styled.div.attrs({
   className:
-    'bg-white flex flex-column items-center justify-between ph4 page-padding montserrat w-100',
+    'bg-light-green flex flex-column items-center justify-between ph4 page-padding montserrat w-100',
 })`
 `
 export default StyledExamples
--- src/pageSections/Testimonials/Testimonials.style.js
+++ src/pageSections/Testimonials/Testimonials.style.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 
 const StyledTestimonials = styled.div.attrs({
   className:
-    'bg-light-green flex flex-column items-center justify-between ph4 page-padding montserrat w-100',
+    'bg-white flex flex-column items-center justify-between ph4 page-padding montserrat w-100',
 })``
 export default StyledTestimonials
