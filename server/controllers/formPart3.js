404f566600dd8e4ced1916fa54fd731ad16f2152
Ben Newman <benjamin-newman@live.co.uk>
2018-10-18 18:41:32 +0100

    getLeadId works > #177

--- server/controllers/formPart3.js
+++ server/controllers/formPart3.js
@@ -1,9 +1,9 @@
 const { postNewUser } = require('../queries/postData/');
+const { getLeadId } = require('../queries/getData/');
 
 exports.post = (req, res) => {
   console.log(req.body);
-  postNewUser(req.body).then((emailAddress) => {
-    console.log(emailAddress);
-  });
+  console.log(res.body);
+  postNewUser(req.body).then((emailAddress) => { getLeadId(emailAddress).then(ids => console.log(ids)); });
   res.sendStatus(200);
 };
--- server/queries/getData/getLeadId.js
+++ server/queries/getData/getLeadId.js
@@ -1,31 +1,29 @@
 const base = require('../../dbConnection');
 
-// const getLeadId = (emailAddress) => {
-//   console.log('getLeadId query');
-//   return new Promise((resolve, reject) => {
-//     let allRecords = [];
-//     const leadIds = [];
-//     base('leads')
-//       .select({
-//         filterByFormula: `AND(email='${emailAddress}')`
-//         ,
-//       })
-//       .eachPage(
-//         (records, fetchNextPage) => {
-//           allRecords = [...allRecords, ...records];
-//           fetchNextPage();
-//         },
-//         (err) => {
-//           if (err) {
-//             reject(err);
-//           } else {
-//             allRecords.forEach(record => leadIds.push(record.id));
-//             resolve(leadIds);
-//           }
-//         },
-//       );
-//   });
-// };
+const getLeadId = emailAddress => new Promise((resolve, reject) => {
+  let allRecords = [];
+  const leadIds = [];
+  base('leads')
+    .select({
+      filterByFormula: `AND(email='${emailAddress}')`
+      ,
+    })
+    .eachPage(
+      (records, fetchNextPage) => {
+        allRecords = [...allRecords, ...records];
+        fetchNextPage();
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          allRecords.forEach(record => leadIds.push(record.id));
+          resolve(leadIds);
+        }
+      },
+    );
+});
 
+getLeadId('testmatt@gmail.com').then(leadIds => console.log(leadIds));
 
 module.exports = getLeadId;
