6e88387d0d85e8d395ab3bf2cecbb308a8e75070
DenisKent <kentdenis6@hotmail.co.uk>
2018-10-18 19:17:01 +0100

    Added leademails to set timeout

--- server/app.js
+++ server/app.js
@@ -3,13 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const controllers = require('./controllers');
 const pairingEmail = require('./emails/pairingEmail');
+const leadEmail = require('./emails/leadEmail');
 
 
 const app = express();
 app.set('PORT', process.env.PORT || 5000);
 
 
-setInterval(pairingEmail, 1000 * 60 * 60);
+setInterval(() => {
+  pairingEmail();
+  leadEmail();
+}, 1000 * 60 * 60);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
