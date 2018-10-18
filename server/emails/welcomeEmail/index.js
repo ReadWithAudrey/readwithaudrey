491dff368f46d0e6cec06dfd6d09f37e48a9e6db
DenisKent <kentdenis6@hotmail.co.uk>
2018-10-18 12:36:41 +0100

    removed commented code

--- server/emails/welcomeEmail/index.js
+++ server/emails/welcomeEmail/index.js
@@ -1,4 +1,2 @@
-const newUser = require('../newUser');
-
 // Audrey users mailing list id = 5541826
-// newUser('tadadaesaat@test.com', 'test', 'test', 5541826);
+const newUser = require('../newUser');
