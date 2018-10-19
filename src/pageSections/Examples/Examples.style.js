4c73c3a0147ac0a2b45911d94a9df5f61614ab6b
Kate Beard <asouthernbellein@gmail.com>
2018-10-19 12:36:57 +0100

    Padding and vertical spacing

--- src/pageSections/Examples/Examples.style.js
+++ src/pageSections/Examples/Examples.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const StyledExamples = styled.div.attrs({
   className:
-    'bg-light-green flex flex-column items-center justify-between ph4 page-padding montserrat w-100',
+    'bg-light-green flex flex-column items-center justify-between ph4 ph6-ns page-padding pv4 montserrat w-100',
 })`
 `
 export default StyledExamples
