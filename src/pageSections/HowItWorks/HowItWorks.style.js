38b6e655f5a9e387205da2c8771727add7843fbe
Ben Newman <benjamin-newman@live.co.uk>
2018-11-22 12:28:38 +0000

    removed the media query > #303

--- src/pageSections/HowItWorks/HowItWorks.style.js
+++ src/pageSections/HowItWorks/HowItWorks.style.js
@@ -3,9 +3,5 @@ import styled from 'styled-components'
 const StyledHowItWorks = styled.div.attrs({
   className:
     'bg-white flex flex-column items-center justify-between ph4 ph6-ns page-padding pv4 montserrat w-100',
-})`
-  @media (max-width: 480px) {
-    min-height: 1400px;
-  }
-`
+})``
 export default StyledHowItWorks
