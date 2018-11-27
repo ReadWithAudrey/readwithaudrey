0c6b02e14f4d670022042f682cb63bbc327a2011
Ben Newman <benjamin-newman@live.co.uk>
2018-11-27 00:00:45 +0000

    removed mini_book_bio from code > #326

--- server/queries/getData/getPairs.test.js
+++ server/queries/getData/getPairs.test.js
@@ -2,6 +2,7 @@ const getNewPairs = require('./getNewPairs');
 const { twoNewPairs, noNewPairs, airtableConnectionErr } = require('../../../__mocks__/json');
 
 test('getPairs :: get all valid pairs', () => {
+  expect.assertions(1);
   getNewPairs().then((data) => {
     expect(data.length).toEqual(2);
   });
