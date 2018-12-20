f7296985a789c951f30c4b2b7f2ab86f090b4de9
Ben Newman <benjamin-newman@live.co.uk>
2018-12-20 20:29:54 +0000

    minor changes > #343

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -10,12 +10,5 @@ const baseGeneral = BaseId => Airtable.base(BaseId);
 
 const base = baseGeneral(process.env.AIRTABLE_BASE);
 const baseAdmin = baseGeneral(process.env.AIRTABLE_BASE_ADMIN);
-// const base = Airtable.base(process.env.AIRTABLE_BASE);
-
-const organisations = [
-  { organisation: 'admin', orgCode: 100000, base: 'appsSVU9iGr00K1v4' },
-  { organisation: 'test', orgCode: 100100, base: 'appsSVU9iGr00K1v4' },
-  { organisation: 'FAC', orgCode: 100101, base: 'appcbskDQClM2MGhA' },
-];
 
 module.exports = { base, baseAdmin, baseGeneral };
