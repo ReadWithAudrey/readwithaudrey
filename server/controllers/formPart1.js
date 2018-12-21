622d72fe23bbbe5519f5e0a26a705c706bf5f223
Ben Newman <benjamin-newman@live.co.uk>
2018-12-21 11:25:27 +0000

    Error message displays if code is incorrect > #341

--- server/controllers/formPart1.js
+++ server/controllers/formPart1.js
@@ -6,14 +6,14 @@ const checkForAndAddLead = (base1, lead, res) => {
   checkUsersTable(base1, lead)
     .then((uniqueEmail) => {
       if (!uniqueEmail) {
-        res.end('Email already in the users table');
+        res.end('email exists');
         return Promise.reject(new Error('Email already in the users table'));
       }
       return checkLeadTable(base1, lead);
     })
     .then((uniqueLead) => {
       if (!uniqueLead) {
-        res.end('Lead already in the leads table');
+        res.end('Lead exists');
         return Promise.reject(new Error('Lead already in the leads table'));
       }
       return createLead(base1, lead);
@@ -23,7 +23,8 @@ const checkForAndAddLead = (base1, lead, res) => {
       res.end('Added the lead to the leads table');
     })
     .catch((err) => {
-      res.end(err);
+      console.log(err);
+      res.end('server error');
     });
 };
 
@@ -36,9 +37,7 @@ exports.post = (req, res) => {
       .then((baseId) => {
         console.log(baseId);
         if (baseId === null) {
-          res.end(
-            'No organisation with that code. Contact your ambassador or sign up as a general user',
-          );
+          res.end('No organisation with that code');
           return Promise.reject(
             new Error(
               'No organisation with that code. Contact your ambassador or sign up as a general user',
@@ -49,10 +48,11 @@ exports.post = (req, res) => {
         checkForAndAddLead(base1, lead, res);
       })
       .catch((err) => {
-        res.end(err);
+        console.log(err);
+        res.end('server error');
       });
   } else {
-    console.log('not got a code - business as usual');
+    console.log('not got a code - business as usual', base, lead, res.body);
     checkForAndAddLead(base, lead, res);
   }
 };
