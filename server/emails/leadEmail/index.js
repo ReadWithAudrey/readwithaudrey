54e0aa1427afca1f9749b633a8eb20e14374223f
Ben Newman <benjamin-newman@live.co.uk>
2019-01-03 13:47:22 +0000

    cross origin sorted and email functions now accept email as arg > #344

--- server/emails/leadEmail/index.js
+++ server/emails/leadEmail/index.js
@@ -2,12 +2,12 @@ const { getLeads } = require('../../queries/getData');
 const { updateLeadEmailSent } = require('../../queries/postData');
 const { sendLeadEmail } = require('./sendLeadEmail');
 
-const sendLeadEmails = (base) => {
+const sendLeadEmails = (base, ambassadorEmail) => {
   getLeads(base)
     .then((leads) => {
       if (leads.length > 0) {
         leads.forEach((lead) => {
-          sendLeadEmail(lead)
+          sendLeadEmail(lead, ambassadorEmail)
             .then(() => updateLeadEmailSent(base, lead.id))
             .then(res => console.log('success', res))
             .catch(err => console.log('Error in the lead email:', err));
