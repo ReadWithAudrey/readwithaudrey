67024f63413412273c249d350eb3a2e1a14dba3b
Ben Newman <benjamin-newman@live.co.uk>
2019-01-21 16:27:23 +0000

    minor changes to the tests > #357

--- server/queries/getData/getPairs.test.js
+++
@@ -1,9 +0,0 @@
-// const getNewPairs = require('./getNewPairs');
-// const { twoNewPairs, noNewPairs, airtableConnectionErr } = require('../../../__mocks__/json');
-//
-// test('getPairs :: get all valid pairs', () => {
-//   expect.assertions(1);
-//   getNewPairs().then((data) => {
-//     expect(data.length).toEqual(2);
-//   });
-// });
