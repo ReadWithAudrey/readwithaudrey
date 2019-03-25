ad3b5846522b93350a337ddfda71c0ceb3ea8a96
bennewman90 <benjamin-newman@live.co.uk>
2019-03-25 23:06:39 +0000

    can now upload images > #385

--- src/pageSections/HowItWorks/HowItWorks.style.js
+++ src/pageSections/HowItWorks/HowItWorks.style.js
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 
 const StyledHowItWorks = styled.div.attrs({
   className:
-    'bg-white flex flex-column items-center justify-between ph4 ph6-ns page-padding pv4 montserrat w-100',
+    'bg-white flex flex-column items-center justify-between ph4 ph6-ns page-padding pv4 montserrat w-100 ',
 })``
 export default StyledHowItWorks
