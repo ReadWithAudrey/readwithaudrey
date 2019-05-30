a7ce2bc7d79f5a244ae8f97c6bc90a08638dae57
bennewman90 <benjamin-newman@live.co.uk>
2019-05-30 21:39:57 +0100

    added blog and instagram links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { StyledFooter, Copyright } from './Footer.style'
+import instagram from '../../images/instagram-mid-grey.png'
+
 
 const Footer = () => (
   <StyledFooter>
     <Copyright>
-      <p className="mid-gray ph2 ph4-ns f5">&copy; Audrey</p>
-      <Link to="/terms/" className="no-underline mid-gray ph2 ph4-ns">
-        Terms
-      </Link>
-      <Link to="/privacy/" className="no-underline mid-gray ph2 ph4-ns">
-        Privacy
-      </Link>
-      <Link to="/contact/" className="no-underline mid-gray ph2 ph4-ns">
+        <p className="mid-gray ph1 ph4-ns f5">&copy; Audrey</p>
+
+      <Link to="/contact/" className="no-underline mid-gray pr2 pl4 ph4-ns">
         Contact
       </Link>
+      <Link to="/blog/" className="no-underline mid-gray pr4 pl4 ph4-ns">
+        Blog
+      </Link>
+      <a href="https://www.instagram.com/readwithaudrey/" className="no-underline mid-gray ph2 ph4-ns">
+      <img src={instagram} alt="book icon" className="h2 ph1" />
+
+      </a>
+    </Copyright>
+    <Copyright>
+      <Link to="/terms/" className="no-underline mid-gray ph2 ph4-ns f6">
+        Terms
+      </Link>
+      <Link to="/privacy/" className="no-underline mid-gray ph2 ph4-ns f6 ">
+        Privacy
+      </Link>
+
+   
     </Copyright>
   </StyledFooter>
+  
 )
 
 export default Footer
